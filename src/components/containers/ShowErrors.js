3a3766bdf64d3687263d701079cc64c6b9310129
Robert Pirritano <rpirritano@gmail.com>
2018-08-26 13:12:06 -0700

    connected error component to store

--- src/components/containers/ShowErrors.js
+++ src/components/containers/ShowErrors.js
@@ -1,5 +1,33 @@
-import ShowErrors from '../ui/ShowErrors'
+//a container component using the connect function that will map the state of errors to the ShowErrors, and
+//it will also map the dispatch function to any onClearError events.
 
-export default () =>
-	<ShowErrors errors={['sample error']}
-						  onClearError={index => console.log('todo: clear error at', index)} />
+//in console can create an error to test with:
+//store.dispatch({ type: "ADD_ERROR", payload: "Something went wrong"})
+
+
+import ShowErrors from '../ui/ShowErrors'
+import { clearError } from '../../actions'
+import { connect } from 'react-redux'
+
+const mapStateToProps = state => {
+
+	return {
+		errors: state.errors
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+
+	return {
+		onClearError(index) {
+			dispatch(
+				clearError(index)
+			)
+		}
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowErrors)
+
+//	<ShowErrors errors={['sample error']}
+//						  onClearError={index => console.log('todo: clear error at', index)} />
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import storeFactory from './store'  //THIS is where we integrate the store
 import { Provider } from 'react-redux' //tool provided to integrate/incorporate the store into the routes
 //This component is for wrapping around any component tree and place the store in Context.
 //Context is a feature, that will allow any child React component to interact with the store, if they want to.
+import { addError } from './actions'
 
 const initialState = (localStorage["redux-store"]) ?
     JSON.parse(localStorage["redux-store"]) :
@@ -14,6 +15,13 @@ const initialState = (localStorage["redux-store"]) ?
 
 const saveState = () =>
     localStorage["redux-store"] = JSON.stringify(store.getState())
+
+//creating funcntion to handle errors and will pass it the the addEventListener  so we can save as log in state
+const handleError = error => {
+  store.dispatch(
+    addError(error.message)
+  )
+}
 
 //create instance of store and give it our initialState
 const store = storeFactory(initialState)
@@ -25,6 +33,9 @@ window.React = React
 //for development pusposes, add to global window in order to interect via console
 window.store = store
 
+//add event listener to listen for errors
+window.addEventListener("error", handleError)
+
 //wrap the routes inside the provider component provided by react-redux
 render(
 	<Provider store={store}>
@@ -32,3 +43,6 @@ render(
 	</Provider>,
   document.getElementById('react-container')
 )
+
+//to test the addError and addEventListener for the error
+//foo = bar 
